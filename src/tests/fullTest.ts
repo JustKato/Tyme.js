afe37e1f782e30dc682cf7a6b30ab1a1771b60e3
Kato Twofold <contact@justkato.me>
2022-05-07 14:35:15 +0300

    + Tests

--- src/tests/fullTest.ts
+++ src/tests/fullTest.ts
@@ -2,8 +2,20 @@ import TymeJS from "..";
 
 const a = new TymeJS();
 
+console.log("🌍", a.format("m/d/Y"));
 console.log(a.format("d/m/Y"));
 console.log(a.format("d/m/Y h:i A"));
+console.log(a.format("d/m/Y H:i:s"));
+
+console.log("\n🇺🇸\n", a.format("m/d/Y"));
+console.log(a.format("m/d/Y"));
+console.log(a.format("m/d/Y h:i A"));
+console.log(a.format("m/d/Y H:i:s"));
+
+console.log("\n🖥\n", a.format("m/d/Y"));
 console.log(a.format("Y-m-d"));
 console.log(a.format("Y-m-d H:i:s"));
+
+console.log(`\n`);
+
 
